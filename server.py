0744c3a342ee7e5726b1a688a361465d21d820a4
Daekllans <theo.naedele@googlemail.com>
2023-06-23 15:29:24 +0200

    Some updates

--- server.py
+++ server.py
@@ -1,19 +1,37 @@
-from fastapi import FastAPI
+import os
+import uvicorn
+from fastapi import FastAPI, Query
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
-
-# Mount the "static" directory to serve static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
+runningDir = os.path.dirname(os.path.abspath(__file__))
+staticDir = os.path.join(runningDir, "static")
 
 
-@app.get("/")
-async def get_index():
-    return FileResponse("static/index.html")
+app = FastAPI(title = "DatabaseUIWebservice")
+api = FastAPI(title="DatabaseUIApi")
 
+app.connection = {
+    "IpAdress": "192.168.178.54",
+    "Port" : 11000,
+    "BufferSize" : 1024,
+    "Timeout": 5.0
+}
+
+def Main():
+    app.mount("/api", api)
+    app.mount("/", StaticFiles(directory=staticDir, html=True), name = "static")
+    uvicorn.run(app,port=8000)
+
+@api.get("/getNewMarkdownContent")
+def getNewMarkdownContent():
+    file = open(staticDir+"/testMarkdown.md","rt")
+    fileText = file.read()
+    fileText.replace("\"","'")
+    print(fileText)
+    file.close()
+    return fileText
 
 if __name__ == "__main__":
-    import uvicorn
+    Main()
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
