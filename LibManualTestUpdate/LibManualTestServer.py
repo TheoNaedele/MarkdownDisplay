0fd1755ad5e5fcd61fc524ef47baf6bc56f70635
Daekllans <theo.naedele@googlemail.com>
2023-08-04 15:25:37 +0200

    Styling, input for fail reasons.

--- LibManualTestUpdate/LibManualTestServer.py
+++ LibManualTestUpdate/LibManualTestServer.py
@@ -3,12 +3,14 @@ import uvicorn
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import FileResponse, Response
 from fastapi.staticfiles import StaticFiles
+from typing import List
 
 runningDir = os.path.dirname(os.path.abspath(__file__))
 staticDir = os.path.join(runningDir, "static")
 
+#maybe append test steps here?
 test_steps = [
-    {"step": "Perform test step 1", "image": None},
+    {"step": "Perform test step 1 \n This is a new line", "image": None},
     {"step": "Perform test step 2", "image": "C:/Users/TheoN/Desktop/Work/TestChatGPT/MarkdownDisplay/LibManualTestUpdate/static/8166acd8-5f9c-44eb-8126-ff9dac111289.jpg"},
     # Add more test steps as needed
 ]
@@ -29,19 +31,28 @@ def Main():
     uvicorn.run(app,port=8000)
 
 # Store connected WebSocket clients
-connected_clients = set()
+#connected_clients = set()
+connected_clients: List[WebSocket] = []
+myWebsocket: WebSocket = None
 
 @app.websocket("/ws/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    connected_clients.add(websocket)
+    connected_clients.append(websocket)
+    #connected_clients.add(websocket)
     try:
         for step in test_steps:
             await websocket.send_json(step)
-            response = await websocket.receive_text()
+            response = await websocket.receive_json()
             print(response)
+            #await sendTeststepToFrontend(step)
     except:
         connected_clients.remove(websocket)
+
+async def sendTeststepToFrontend(teststep: dict):
+    await connected_clients[0].send_json(test_steps[0])
+    return
+
 
 @api.get("/getImageByPath")
 def getImageByPath(path: str):
